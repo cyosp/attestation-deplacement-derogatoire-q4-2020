183e639358fb6c9bccb0428197399d486706420f
CYOSP <cyosp@cyosp.com>
2020-11-03 07:48:55 +0100

    Add script to update dist folder

---
+++ distUpdate.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd dist
+
+for i in $(ls)
+do
+  if [[ $i != "index.html" ]] && [[ $i != "update.bash" ]]
+  then
+    sed -i "s|$i|attestation-deplacement-derogatoire-q4-2020/$i|g" *.html *.js *.css
+  fi
+
+done
+
+cd $OLDPWD
